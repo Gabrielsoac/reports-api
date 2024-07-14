727ac2374c1d1cd7cc34d4d4e56f052902e7e84a
Gabriel Soares <gabrielsoacc@gmail.com>
2024-07-13 22:18:57 -0300

    Created: Entity Author

--- reports-api/src/main/java/com/reports/model/entities/Author.java
+++ reports-api/src/main/java/com/reports/model/entities/Author.java
@@ -1,9 +1,30 @@
 package com.reports.model.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
-@Table("author")
-@Entity
+@Table(name = "author")
+@Entity(name = "author")
 public class Author {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+
+    public Author(String name){
+        this.name = name;
+    }
+    public Author() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
--- reports-api/src/main/java/com/reports/model/entities/Report.java
+++ reports-api/src/main/java/com/reports/model/entities/Report.java
@@ -1,4 +1,50 @@
 package com.reports.model.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
 public class Report {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private LocalDate date;
+    private String title;
+    private Author author;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
 }
+

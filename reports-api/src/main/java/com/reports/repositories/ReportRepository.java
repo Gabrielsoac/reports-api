08678081c4901ca35d36570e5addd318ae366dbc
Gabriel Soares <gabrielsoacc@gmail.com>
2024-07-14 11:53:51 -0300

    Created: Report Repository Extended for JPARepository for acess DB

---
+++ reports-api/src/main/java/com/reports/repositories/ReportRepository.java
@@ -0,0 +1,9 @@
+package com.reports.repositories;
+
+import com.reports.model.entities.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, String> {
+}

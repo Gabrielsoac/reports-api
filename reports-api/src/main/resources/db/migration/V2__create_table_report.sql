963a5383a4251f818aa6a487a81d60a45d28370b
Gabriel Soares <gabrielsoacc@gmail.com>
2024-07-13 22:18:18 -0300

    Migrattion: Create Table Report

---
+++ reports-api/src/main/resources/db/migration/V2__create_table_report.sql
@@ -0,0 +1,7 @@
+CREATE TABLE report (
+    id varchar(255) UNIQUE NOT NULL PRIMARY KEY,
+    date DATE NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
+);

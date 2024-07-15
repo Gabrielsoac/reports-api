d2d55d690e98f8cde333a7c9cf6c6d3d201bbb39
Gabriel Soares <gabrielsoacc@gmail.com>
2024-07-15 20:17:51 -0300

    Change: Alter Author Class for String

--- reports-api/src/main/java/com/reports/DTOs/RequestReportDataDTO.java
+++ reports-api/src/main/java/com/reports/DTOs/RequestReportDataDTO.java
@@ -1,8 +1,6 @@
 package com.reports.DTOs;
 
-import com.reports.model.entities.Author;
-
 public record RequestReportDataDTO(String title,
-                                   Author author,
+                                   String author,
                                    String description) {
 }

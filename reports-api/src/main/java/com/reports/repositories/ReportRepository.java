6648d271d476359582d9443174f6720001383c1d
Gabriel Soares <gabrielsoacc@gmail.com>
2024-07-15 20:22:25 -0300

    Change: Added method for find Report by Date and Title

--- reports-api/src/main/java/com/reports/repositories/ReportRepository.java
+++ reports-api/src/main/java/com/reports/repositories/ReportRepository.java
@@ -2,8 +2,12 @@ package com.reports.repositories;
 
 import com.reports.model.entities.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.time.LocalDate;
+
+
 public interface ReportRepository extends JpaRepository<Report, String> {
+
+    Report findReportByDateAndTitle(LocalDate date, String title);
+
 }

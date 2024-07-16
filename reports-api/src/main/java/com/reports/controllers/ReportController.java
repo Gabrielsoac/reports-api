4a75d8836daa649967ba6a3f756c7b92d8958e09
Gabriel Soares <gabrielsoacc@gmail.com>
2024-07-15 21:15:52 -0300

    Change: Path error searching report, changed for to search by raw JSON

--- reports-api/src/main/java/com/reports/controllers/ReportController.java
+++ reports-api/src/main/java/com/reports/controllers/ReportController.java
@@ -1,9 +1,44 @@
 package com.reports.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.reports.DTOs.DateAndTitleRequestReportDTO;
+import com.reports.DTOs.RequestEditReportDTO;
+import com.reports.DTOs.RequestReportDataDTO;
+import com.reports.services.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/reports")
+@RequiredArgsConstructor
 public class ReportController {
 
-    
+    private final ReportService reportService;
+
+    @GetMapping
+    public ResponseEntity getReports(){
+        return ResponseEntity.status(200).body(reportService.findAllReports().reportList());
+    }
+    @GetMapping("/find-report")
+    public ResponseEntity getReport(@RequestBody DateAndTitleRequestReportDTO data){
+        return ResponseEntity.status(200).body(reportService.findReport(data.date(), data.title()));
+    }
+
+    @PostMapping
+    public ResponseEntity createReport(@RequestBody RequestReportDataDTO dataDTO){
+        return ResponseEntity.status(200).body(reportService.createReport(dataDTO));
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateReport(@RequestBody RequestEditReportDTO requestEditReportDTO){
+        return ResponseEntity.status(200).body(reportService.editReport(requestEditReportDTO));
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity deleteReport(@RequestBody DateAndTitleRequestReportDTO deleteReportDTO){
+        return ResponseEntity.status(200).build();
+    }
 }

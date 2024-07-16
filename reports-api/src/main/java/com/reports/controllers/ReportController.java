4f5898e8f5ebcf7075989b2b83b4fcbd1fd708b2
Gabriel Soares <gabrielsoacc@gmail.com>
2024-07-16 11:45:42 -0300

    Change: Return for new DTOS

--- reports-api/src/main/java/com/reports/controllers/ReportController.java
+++ reports-api/src/main/java/com/reports/controllers/ReportController.java
@@ -1,13 +1,13 @@
 package com.reports.controllers;
 
-import com.reports.DTOs.DateAndTitleRequestReportDTO;
-import com.reports.DTOs.RequestEditReportDTO;
-import com.reports.DTOs.RequestReportDataDTO;
+import com.reports.DTOs.*;
 import com.reports.services.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reports")
@@ -17,28 +17,28 @@ public class ReportController {
     private final ReportService reportService;
 
     @GetMapping
-    public ResponseEntity getReports(){
+    public ResponseEntity<List<ResponseReportDTO>> getReports(){
         return ResponseEntity.status(200).body(reportService.findAllReports().reportList());
     }
     @GetMapping("/find-report")
-    public ResponseEntity getReport(@RequestBody DateAndTitleRequestReportDTO data){
+    public ResponseEntity<ResponseReportDTO> getReport(@RequestBody RequestDateAndTitleReportDTO data){
         return ResponseEntity.status(200).body(reportService.findReport(data.date(), data.title()));
     }
 
     @PostMapping
-    public ResponseEntity createReport(@RequestBody RequestReportDataDTO dataDTO){
+    public ResponseEntity<ResponseReportDTO> createReport(@RequestBody RequestReportDataDTO dataDTO){
         return ResponseEntity.status(200).body(reportService.createReport(dataDTO));
     }
 
     @PutMapping
     @Transactional
-    public ResponseEntity updateReport(@RequestBody RequestEditReportDTO requestEditReportDTO){
+    public ResponseEntity<ResponseReportEditDTO> updateReport(@RequestBody RequestEditReportDTO requestEditReportDTO){
         return ResponseEntity.status(200).body(reportService.editReport(requestEditReportDTO));
     }
 
     @DeleteMapping
     @Transactional
-    public ResponseEntity deleteReport(@RequestBody DateAndTitleRequestReportDTO deleteReportDTO){
+    public ResponseEntity deleteReport(@RequestBody RequestDateAndTitleReportDTO deleteReportDTO){
         return ResponseEntity.status(200).build();
     }
 }
